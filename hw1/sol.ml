9d78fe78f1741d23e6e04cd7e617fe053e97a1e8
Jaeyong Lim (임재용 | 林在庸) <jaylim61@icloud.com>
2026-09-25 12:10:52 +0900

    Solve problem 12

--- hw1/sol.ml
+++ hw1/sol.ml
@@ -108,3 +108,35 @@ let two = SUCC one
 let three = SUCC two
 let four = SUCC three
 let five = SUCC four
+
+(* Problem 12 *)
+type formula =
+| True
+| False
+| Not of formula
+| AndAlso of formula * formula
+| OrElse of formula * formula
+| Imply of formula * formula
+| Equal of exp * exp
+and exp =
+| Num of int
+| Plus of exp * exp
+| Minus of exp * exp
+
+let rec eval f =
+  match f with
+  | True -> true
+  | False -> false
+  | Not p -> not (eval p)
+  | AndAlso (p, q) -> (eval p) && (eval q)
+  | OrElse (p, q) -> (eval p) || (eval q)
+  | Imply (p, q) -> (not (eval p)) || (eval q)
+  | Equal (p, q) ->
+    let rec evalexp e =
+      match e with
+      | Num n -> n
+      | Plus (m, n) -> (evalexp m) + (evalexp n)
+      | Minus (m, n) -> (evalexp m) - (evalexp n)
+    in (evalexp p) = (evalexp q)
+
+      
